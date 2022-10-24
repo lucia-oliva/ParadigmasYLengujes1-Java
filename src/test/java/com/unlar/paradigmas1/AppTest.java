7060258325b33e33ca8a5fe9d433df41d8a90071
Lucia Oliva <lucia222lr@gmail.com>
2022-10-24 20:15:47 -0300

    crear proyecto

---
+++ src/test/java/com/unlar/paradigmas1/AppTest.java
@@ -0,0 +1,20 @@
+package com.unlar.paradigmas1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
